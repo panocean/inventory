f32cb4929da4daa8fde97fde222f21dc55f212c7
Power Administrative Account <Power@poocng.local>
2019-05-16 13:28:00 +0100

    add test json data

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 
 var express = require('express');
+var mongoose = require('mongoose');
+const request = require('request');
+const path = require('path');
+const fs = require('fs');
+const csv=require('csvtojson');
+
+
+
 var server = express();
+
+mongoose.connect('mongodb://localhost/poocInventory', {useNewUrlParser: true})
+.then(() => {
+    console.log(`Connected to DB poocInventory`)
+}).catch((err) => {
+    console.log(err.message);
+})
+
+// csv()
+//   .fromStream(request.get(fs.readFile(path.join(__dirname,'HQASSETEST.xlsx'))))
+//   .subscribe((json) => {
+//       return new Promise((resolve, reject) => {
+
+//       })
+//   })
 
 server.get('/whatever', function (req, res) {
     res.send('Hello World')
@@ -8,8 +31,11 @@ server.get('/whatever', function (req, res) {
 
 
 server.get('/devices', function (req, res) {
-    res.send('This route will return all panocean devices')
+    res.send('This route will return all panocean all devices')
 })
+
+
+server.use(express.json());
 
 server.listen(3000, function() {
     console.log(`server running on port 3000`)
