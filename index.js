986979a0130a5d3dd20f94baadbf6b811083a258
jidemobell <jidd32@gmail.com>
2019-05-23 16:53:11 +0100

    fetch inventory data from csv

--- index.js
+++ index.js
@@ -1,42 +1,82 @@
-
 var express = require('express');
 var mongoose = require('mongoose');
 const request = require('request');
 const path = require('path');
 const fs = require('fs');
-const csv=require('csvtojson');
+const csv = require('fast-csv');
+const csvj = require('csvtojson');
+const mongoImport = require('mongoimport');
 
 
 
 var server = express();
 
-mongoose.connect('mongodb://localhost/poocInventory', {useNewUrlParser: true})
-.then(() => {
+
+
+mongoose.connect('mongodb://localhost/poocInventory', {
+    useNewUrlParser: true
+  })
+  .then(() => {
     console.log(`Connected to DB poocInventory`)
-}).catch((err) => {
+    mongoose.connection.db.listCollections().toArray()
+      .then(doc => {
+        if (!doc[0].name === "devices") {
+          csvj()
+            .fromFile(path.join(__dirname, 'hq_asset_csv.csv'))
+            .then(jsonObj => {
+              console.log("the data", JSON.stringify(jsonObj, undefined, 2))
+              const config = {
+                fields: jsonObj,
+                db: 'poocInventory',
+                collection: 'devices',
+                host: 'localhost:27017',
+                callback: (err, db) => {
+                  if (err) console.log("import error: ", err)
+                }
+              }
+              mongoImport(config);
+            })
+            .catch(err => console.log(err))
+        }
+        console.log("collection already exists");
+      })
+  }).catch((err) => {
     console.log(err.message);
-})
+  })
 
-// csv()
-//   .fromStream(request.get(fs.readFile(path.join(__dirname,'HQASSETEST.xlsx'))))
-//   .subscribe((json) => {
-//       return new Promise((resolve, reject) => {
 
-//       })
-//   })
+// const db = mongoose.connection;
+
+
+
+// csvj()
+//   .fromFile(path.join(__dirname, 'hq_asset_csv.csv'))
+//     .then(jsonObj => {
+//         console.log("the data", JSON.stringify(jsonObj, undefined, 2))
+//         const config = {
+//             fields: jsonObj,
+//             db: 'poocInventory',
+//             collection: 'devices',
+//             host: 'localhost:27017',
+//             callback: (err, db) => {if(err) console.log("import error: ", err)}
+//         }
+//         mongoImport(config);
+//     })
+//     .catch(err => console.log(err))
+
 
 server.get('/whatever', function (req, res) {
-    res.send('Hello World')
+  res.send('Hello World')
 })
 
 
 server.get('/devices', function (req, res) {
-    res.send('This route will return all panocean all devices')
+  res.send('This route will return all panocean all devices')
 })
 
 
 server.use(express.json());
 
-server.listen(3000, function() {
-    console.log(`server running on port 3000`)
-} )
+server.listen(3000, function () {
+  console.log(`server running on port 3000`)
+})
